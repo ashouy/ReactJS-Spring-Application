73d6d2957ef1e6799241717722299e00c369cc0c
ashouy <joaopaulokad@gmail.com>
2022-03-23 22:42:06 -0300

    basic react interface

--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Employee;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
